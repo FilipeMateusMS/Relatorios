13b8fa9fd06c6d9f096c553d686bdc34cbf8578f
Filipe Mateus <146008498+FilipeMateusMS@users.noreply.github.com>
2026-09-17 16:02:59 -0300

    feat: add domain not found exception

---
+++ src/main/java/com/filipe/relatorios/domain/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,6 @@
+package com.filipe.relatorios.domain.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException
+{
+    public RecursoNaoEncontradoException( String mensagem ) { super( mensagem ); }
+}
